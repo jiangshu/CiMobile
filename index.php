315ce0abfed6c6c2725b5c5fccec036f7320c31e
jiangshuguang <jiangshuguangzz@163.com>
2013-05-20 23:28:17 +0800

    整体修改

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+  <head>
+      <meta charset="utf-8"/>
+      <link type="text/css" rel="stylesheet" href="./static/css/common.css"/>
+      <link type="text/css" rel="stylesheet" href="./static/css/index.css"/>
+      <script type="text/javascript" src="./static/js/jquery-1.8.2.js"></script>
+      <script type="text/javascript" src="./static/js/index.js"></script>
+  </head>
+  <body>
+     <div class="header">
+         <div class="logo">CIMobile</div>
+         <div class="nav">
+             <ul>
+                 <li style="background-color: #686868;"><a href="index.php">全部</a></li>
+                 <li><a href="filter.php">过滤</a></li>
+                 <li><a href="down.php">下载</a></li>
+             </ul>
+         </div>
+     </div>
+     <div class="body">
+         <table align="center" cellpadding="3" width="100%" border="1" id="mobileList" style=" border-collapse:collapse;table-layout: fixed;word-break: break-all;text-align:center">
+             <tr bgcolor="#2185ff" style="color:#fff;">
+                 <td width="5%">序号</td>
+                 <td width="10%">mobile型号</td>
+                 <td width="10%">系统版本</td>
+                 <td width="10%">mobile别名</td>
+                 <td width="65%">浏览器操作</td>
+<!--                 <td width="25%">安装包操作</td>-->
+             </tr>
+         </table>
+     </div>
+     <div class="update ge_button">更新</div>
+
+  </body>
+</html>
